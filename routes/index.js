f84b6d862df886d4c832d63b22e7a9d37201aa8c
mehtanikita <nikitamehta1995@gmail.com>
2020-03-14 15:46:16 -0700

    forgot to add a file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
+	.get('/', function(req, res, next) {
+	  res.render('index', { title: 'Express' });
+	})
+	.get('/db', async (req, res) => {
+	    try {
+	      const client = await pool.connect()
+	      const result = await client.query('SELECT * FROM test_table');
+	      const results = { 'results': (result) ? result.rows : null};
+	      res.render('pages/db', results );
+	      client.release();
+	    } catch (err) {
+	      console.error(err);
+	      res.send("Error " + err);
+	    }
+	  });
 
 module.exports = router;
